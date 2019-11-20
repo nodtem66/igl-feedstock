b33498f5d12795b0384bfdd33844380a2a94e1b9
Teseo Schneider <teseo@bluewin.ch>
2019-11-20 11:20:47 -0500

    removed only gl flag

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,4 @@
-set "CFLAGS= -MD"
-set "CXXFLAGS= -MD"
+REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
+REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
+set CXXFLAGS=%CXXFLAGS:-GL=%
+set CFLAGS=%CFLAGS:-GL=%
